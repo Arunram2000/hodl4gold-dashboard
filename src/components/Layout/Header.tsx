86810459070e121ce170baed1372a5dc98290165
Arunachalam N <arunachalam@Arunachalams-MacBook-Pro.local>
2022-04-14 19:37:27 +0530

    choke:responsive

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Header.scss";
 import logo from "../../assets/images/logo_dark.png";
 import dashboard from "../../assets/icons/dashboard.svg";
+// import arrow from "../../assets/icons/arrow.svg";
+// import app from "../../assets/icons/app.svg";
 import Button from "../Button";
 import { useWeb3React } from "@web3-react/core";
+import Menu from "../Icons/Menu";
 
 const Header: React.FC = () => {
   const { account } = useWeb3React();
+  const [sidebar, setSidebar] = useState(false);
+
+  const renderHeaderControls = (
+    <div className="header_controls">
+      <Button>
+        {account
+          ? `${account.slice(0, 6)}...${account.slice(account.length - 6)}`
+          : "Connect wallet"}
+      </Button>
+      <Button variant="secondary">Buy H4G Now.</Button>
+    </div>
+  );
+
+  const renderHeaderLinks = (
+    <>
+      <div className="header_lists-links">
+        <div className="search_bar">
+          <input type="search" placeholder="Explore with address" />
+        </div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>Smart Contracts</Link>
+      </div>
+    </>
+  );
+
   return (
     <>
       <header className="header_wrapper pad">
         <section className="mx">
           <div className="header_lists">
             <img src={logo} alt="logo" className="logo" />
-            <div className="header_lists-links">
-              <div className="search_bar">
-                <input type="search" placeholder="Explore with address" />
-              </div>
-              <Link to={"/"}>Home</Link>
-              <Link to={"/"}>Smart Contracts</Link>
-            </div>
+            {renderHeaderLinks}
           </div>
-          <div className="header_controls">
-            <Button>
-              {account
-                ? `${account.slice(0, 6)}...${account.slice(
-                    account.length - 6
-                  )}`
-                : "Connect wallet"}
-            </Button>
-            <Button variant="secondary">Buy H4G Now.</Button>
+          {renderHeaderControls}
+          <div className="header_hamburger" onClick={() => setSidebar(true)}>
+            <Menu />
           </div>
         </section>
       </header>
@@ -42,9 +58,23 @@ const Header: React.FC = () => {
               <img src={dashboard} alt="icon" />
               <span>Dashboard</span>
             </Link>
+            {/* <div className="dropdown_link">
+              <p><img src={app} alt="icon" /><span>Apps <img src={arrow} alt="arrow icon" /></span></p>
+              <div className="dropdown_lists">
+                
+              </div>
+            </div> */}
           </div>
         </section>
       </nav>
+      <section
+        className={sidebar ? "sidebar_backdrop active" : "sidebar_backdrop"}
+        onClick={() => setSidebar(false)}
+      />
+      <aside className={sidebar ? "sidebar active" : "sidebar"}>
+        {renderHeaderLinks}
+        {renderHeaderControls}
+      </aside>
     </>
   );
 };
--- src/pages/Dashboard/Chart.tsx
+++ src/pages/Dashboard/Chart.tsx
@@ -1,12 +1,51 @@
-import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+
+import arrow from "../../assets/icons/arrow.svg";
+
+const timeframeLists = ["monthly", "weekly", "yearly"];
 
 const Chart: React.FC = () => {
+  const [timeframe, setTimeFrame] = useState("monthly");
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className="chart_wrapper">
       <div className="chart-header">
         <h4>Reward distribution vs payouts</h4>
         <section>
-          <h5>TIMEFRAME :</h5> <p>Monthly</p>
+          <h5>TIMEFRAME :</h5>{" "}
+          <div
+            className="timeframe_dropdown"
+            onMouseEnter={() => setIsActive(true)}
+            onMouseLeave={() => setIsActive(false)}
+          >
+            <p className="flex">
+              <span>{timeframe}</span> <img src={arrow} alt="arrow icon" />
+            </p>
+            <AnimatePresence exitBeforeEnter>
+              {isActive && (
+                <motion.section
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className="dropdown_list"
+                >
+                  {timeframeLists.map((t, index) => (
+                    <p
+                      key={index.toString()}
+                      onClick={() => {
+                        setTimeFrame(t);
+                        setIsActive(false);
+                      }}
+                    >
+                      {t}
+                    </p>
+                  ))}
+                </motion.section>
+              )}
+            </AnimatePresence>
+          </div>
         </section>
       </div>
       <div className="chart-content">chart</div>
