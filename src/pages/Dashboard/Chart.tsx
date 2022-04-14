214cd0d89bcb337b4e2a925cf0daa6501f36831b
Arunachalam N <arunachalam@Arunachalams-MacBook-Pro.local>
2022-04-14 20:24:59 +0530

    feat : charts

--- src/pages/Dashboard/Chart.tsx
+++ src/pages/Dashboard/Chart.tsx
@@ -1,7 +1,17 @@
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 import arrow from "../../assets/icons/arrow.svg";
+import { chartData } from "../../data/chart";
 
 const timeframeLists = ["monthly", "weekly", "yearly"];
 
@@ -48,7 +58,43 @@ const Chart: React.FC = () => {
           </div>
         </section>
       </div>
-      <div className="chart-content">chart</div>
+      <div className="chart-content">
+        <ResponsiveContainer width={"100%"} height={300}>
+          <AreaChart
+            data={chartData}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" fontSize={10} />
+            <YAxis fontSize={10} />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="pv"
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
